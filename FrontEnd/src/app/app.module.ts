ae1d21245acac06f81b47721e41401ba21cd6174
diegogieser <diego.gieser@gmail.com>
2022-09-24 07:47:31 -0300

    Carpeta FrontEnd
    Division en carpetas y carga de archivos frontEnd

---
+++ FrontEnd/src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoPFComponent } from './components/logo-pf/logo-pf.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HardskillsComponent } from './cpmponents/hardskills/hardskills.component';
+import { HardskillComponent } from './components/hardskill/hardskill.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    LogoPFComponent,
+    BannerComponent,
+    AcercaDeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    HardskillsComponent,
+    HardskillComponent,
+    ProyectosComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
--- FrontEnd/src/app/components/hardskill/hardskill.component.spec.ts
+++ FrontEnd/src/app/components/hardskill/hardskill.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SocialComponent } from './social.component';
+import { HardskillComponent } from './hardskill.component';
 
-describe('SocialComponent', () => {
-  let component: SocialComponent;
-  let fixture: ComponentFixture<SocialComponent>;
+describe('HardskillComponent', () => {
+  let component: HardskillComponent;
+  let fixture: ComponentFixture<HardskillComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SocialComponent ]
+      declarations: [ HardskillComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(SocialComponent);
+    fixture = TestBed.createComponent(HardskillComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
